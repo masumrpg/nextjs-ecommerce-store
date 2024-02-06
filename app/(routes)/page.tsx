88a6f57421e98877ff7dc8bb0809cbcea0eb4f02
masumrpg <masumrpg@gmail.com>
2024-02-06 17:47:00 +0700

    init

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,24 +4,23 @@ import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
 
+const billBoardId = `${process.env.BILLBOARD_ID}`;
 export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard(billBoardId);
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
